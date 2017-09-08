cadf2ddfd069026da1b1b4f800c36b815b89b780
Dan Tolbert <dtolbert@bandwidth.com>
2017-09-08 14:08:14 -0400

    update

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ const IVR_CHOICE    = '/ivr-choice-endpoint';
 /* Event Handlers */
 const handleIncomingCall = (req, res) => {
     const baseUrl = `http://${req.hostname}`;
-    res.send("hello!");
+    const nextUrl = baseUrl + IVR_CHOICE;
+    const sentence = `Hello! Thanks for calling Raleigh takeout. Please listen closely as our menu options have changed.  For any Pizza, please press 1.  For Bad Daddys Burgers, press 2.`
+    const bxml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Gather requestUrl="${nextUrl}" maxDigits="1">
+        <SpeakSentence gender="female" locale="en_US" voice="susan">${sentence}</SpeakSentence>
+    </Gather>
+</Response>`
+
+    res.send(bxml);
 };
 
 const handleGatherEvent = (req, res) => {
